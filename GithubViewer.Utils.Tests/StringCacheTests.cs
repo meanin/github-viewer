98bdda3eaf7618ba12c1a91977e6a91cf0ab222e
meanin <meanin@wp.pl>
2017-09-13 16:31:28 +0200

    Tests for cache

---
+++ GithubViewer.Utils.Tests/StringCacheTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using GithubViewer.Utils.Services;
+using Xunit;
+
+namespace GithubViewer.Utils.Tests
+{
+    public class StringCacheTests
+    {
+        private readonly StringCache _cache;
+        private readonly string _key;
+
+        public StringCacheTests()
+        {
+            _cache = new StringCache();
+            _key = "key";
+        }
+
+        [Fact]
+        public void Get_WhenPreviouslyNotPutAOnbjectWithKey_ShouldReturnStringEmpty()
+        {
+            // arrange
+
+            // act
+
+            var stringObject = _cache.Get(_key);
+
+            // assert
+
+            stringObject.Should().NotBeNull();
+            stringObject.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Get_WhenPreviouslyPutAOnbjectWithKey_ShouldReturnStringEmpty()
+        {
+            // arrange
+
+            var obj = "object";
+            _cache.Put(_key, obj);
+
+            // act
+
+            var stringObject = _cache.Get(_key);
+
+            // assert
+
+            stringObject.Should().NotBeNull();
+            stringObject.Should().Be(obj);
+        }
+    }
+}
